1d02da608befc2c3bcaea7aafa163100f4d690fe
rahul975337 <tilwani03@gmail.com>
2020-10-18 12:42:42 +0530

    changes done

--- bmicalc/lib/main.dart
+++ bmicalc/lib/main.dart
@@ -12,7 +12,7 @@ class BMICalculator extends StatelessWidget {
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
         textTheme: TextTheme(
-          body1: TextStyle(color: Colors.white),
+          bodyText2: TextStyle(color: Colors.white),
         ),
       ),
       home: InputPage(),
